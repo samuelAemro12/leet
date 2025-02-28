daf54e171a5c11092f8f427caee61b790d8da672
Samuel Aemro Melese <154930921+samuelAemro12@users.noreply.github.com>
2025-02-28 18:15:56 +0300

    Time: 64 ms (26.78%) | Memory: 17.7 MB (89.94%) - LeetSync

---
+++ 43-multiply-strings/multiply-strings.py
@@ -0,0 +1,27 @@
+class Solution:
+
+    def multiply(self, num1: str, num2: str) -> str:
+        if num1 == "0" or num2 == "0":
+            return "0"
+        
+        result = [0] * (len(num1) + len(num2))
+
+        num1 = num1[::-1]
+        num2 = num2[::-1]
+
+        for i in range(len(num1)):
+            for j in range(len(num2)):
+                digit_product = int(num1[i]) * int(num2[j])
+                result[i + j] += digit_product
+                result[i + j + 1] += result[i + j] // 10
+                result[i + j] %= 10
+        while len(result) > 1 and result[-1] == 0:
+            result.pop()
+
+        return ''.join(map(str, result[::-1]))
+
+num1 = "327"
+num2 = "892"
+solution = Solution()
+a = solution.multiply(num1, num2)
+print(a)
