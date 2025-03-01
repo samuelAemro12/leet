9b6e9b06499060d58c0b5acdf8593a98cfb51c88
Samuel Aemro Melese <154930921+samuelAemro12@users.noreply.github.com>
2025-03-01 21:36:01 +0300

    Time: 0 ms (100.00%) | Memory: 17.7 MB (87.49%) - LeetSync

---
+++ 258-add-digits/add-digits.py
@@ -0,0 +1,14 @@
+class Solution:
+    def addDigits(self, num: int) -> int:
+        while num >= 10:
+            sum = 0
+            while num > 0:
+                sum += num % 10
+                num //= 10
+            num = sum
+        return num 
+
+num = 45 
+solution = Solution()
+x = solution.addDigits(num)
+print(x)
