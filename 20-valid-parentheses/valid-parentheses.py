6d8de46ebda7f1747ef248628e7603b865bf4f78
Samuel Aemro Melese <154930921+samuelAemro12@users.noreply.github.com>
2025-03-07 19:00:35 +0300

    Time: 4 ms (8.68%) | Memory: 18 MB (32.43%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        mapping = {')': '(', '}': '{', ']': '['}
+        for char in s:
+            if char in mapping: 
+                top_element = stack.pop() if stack else '#'
+                if mapping[char] != top_element:
+                    return False
+            else:
+                
+                stack.append(char)
+        return not stack
+
+solution = Solution()
+s = "()[]{}"
+result = solution.isValid(s)
+print(result) 
