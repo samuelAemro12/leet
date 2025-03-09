f8966f783c78d828eea4dccf95d6de1d4f6c19fd
Samuel Aemro Melese <154930921+samuelAemro12@users.noreply.github.com>
2025-03-09 22:30:31 +0300

    Time: 0 ms (100.00%) | Memory: 17.7 MB (81.77%) - LeetSync

---
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.py
@@ -0,0 +1,18 @@
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        if not needle:
+            return 0
+        haystack_len = len(haystack)
+        needle_len = len(needle)
+        for i in range(haystack_len - needle_len + 1):
+            if haystack[i:i + needle_len] == needle:
+                return i
+            
+        return -1 
+
+
+solution = Solution()
+haystack = "urequitedMohabett"
+needle = "Mohabett"
+result = solution.strStr(haystack, needle)
+print(result) 
