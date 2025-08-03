b616b65c99903b4e0a7d95fff590965d4c82119c
Samuel Aemro Melese <samuelaemrowork12@gmail.com>
2025-08-04 01:54:54 +0300

    Time: 0 ms (100.00%) | Memory: 17.8 MB (32.32%) - LeetSync

--- 69-sqrtx/sqrtx.py
+++ 69-sqrtx/sqrtx.py
@@ -1,21 +1,30 @@
 class Solution:
-    def mySqrt(self, x: int) -> int:
-        if x < 2:
-            return x          
-        left, right = 2, x // 2         
-        while left <= right:
-            mid = left + (right - left) // 2
-            squared = mid * mid
-            
-            if squared == x:
-                return mid 
-            elif squared < x:
-                left = mid + 1 
-            else:
-                right = mid - 1 
-        
-        return right  
+    def mySqrt(self, x):
+        if x < 0:
+            return 0
+        if x == 0:
+            return 0
+        low = 0
+        high = x
+        answer = 0
 
-sol = Solution()
-print(sol.mySqrt(4))
-print(sol.mySqrt(8))  
+        while low <= high:
+            mid = low + (high - low) // 2
+            mid_squared = mid * mid
+            if mid_squared == x:
+                return mid
+            elif mid_squared < x:
+                ans = mid
+                low = mid + 1
+            else:
+                high = mid - 1
+
+        return ans
+
+solution = Solution()
+
+print(f"Sqrt(4) = {solution.mySqrt(4)}")
+print(f"Sqrt(8) = {solution.mySqrt(8)}")
+print(f"Sqrt(0) = {solution.mySqrt(0)}")
+print(f"Sqrt(1) = {solution.mySqrt(1)}")
+print(f"Sqrt(2147395599) = {solution.mySqrt(2147395599)}")
